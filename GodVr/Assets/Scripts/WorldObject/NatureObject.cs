211dab509e6b49a52dc690a38ba158e882b74700
VidarVigg <vidar.martensson@outlook.com>
2019-09-02 14:32:49 +0200

    Added Scripts for World Objects

---
+++ GodVr/Assets/Scripts/WorldObject/NatureObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class NatureObject : InteractableWorldObject
+{
+    public enum NatureType
+    {
+        Stone,
+        Tree,
+    }
+    public NatureType natureType;
+    int resourceValue;
+}
---
+++ GodVr/Assets/Scripts/WorldObject/Structure.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Structure : InteractableWorldObject
+{
+    public struct ResourceStruct
+    {
+        int value;
+        int resource;// Should be enum
+        public ResourceStruct(int value, int resource)
+        {
+            this.value = value;
+            this.resource = resource;
+        }
+    }
+    public ResourceStruct[] resourceCosts = null;
+    public override void Grab()
+    {
+
+    }
+
+    public override void Place()
+    {
+
+    }
+
+    public override void Throw()
+    {
+
+    }
+}

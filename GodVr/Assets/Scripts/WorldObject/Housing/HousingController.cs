eb7a3c3fcae91ca58bbf54e7cfa81d09dff83bdb
VidarVigg <vidar.martensson@outlook.com>
2019-09-03 10:56:59 +0200

    Added structure for Housing

---
+++ GodVr/Assets/Scripts/WorldObject/Housing/HousingController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class HousingController
+{ 
+
+    #region Fields
+    
+    private HousingMaster housingMaster = null;
+    private HousingConfig housingConfig = null;
+    private HousingData housingData = null;
+
+    #endregion
+
+    #region Constructors
+
+    private HousingController () { }
+    public HousingController(HousingMaster housingMaster, HousingConfig housingConfig, HousingData housingData)
+    {
+        this.housingMaster = housingMaster;
+        this.housingConfig = housingConfig;
+        this.housingData = housingData;
+    }
+
+    #endregion
+
+}
---
+++ GodVr/Assets/Scripts/WorldObject/Housing/HousingMaster.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HousingMaster : Structure
+{
+
+    #region Fields
+
+    [SerializeField] private HousingConfig housingConfig = null;
+    [SerializeField] private HousingData housingData = null;
+    [SerializeField] private HousingController housingController = null;
+
+    #endregion
+
+    #region Methods
+
+    private void Awake()
+    {
+        housingController = new HousingController(this, housingConfig, housingData);
+    }
+
+    #endregion
+
+}

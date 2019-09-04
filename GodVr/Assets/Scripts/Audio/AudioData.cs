a81974201d1c13c99c958950d4f39800acab638b
VidarVigg <vidar.martensson@outlook.com>
2019-09-04 13:51:11 +0200

    Added properties to audio set

--- GodVr/Assets/Scripts/Audio/AudioData.cs
+++ GodVr/Assets/Scripts/Audio/AudioData.cs
@@ -7,7 +7,25 @@ public class AudioData
 
     #region Fields
 
-    [SerializeField] private AudioObject[] audioObjects = new AudioObject[0];
+    [SerializeField] private AudioObject[] audioObjectsMenu = new AudioObject[0];
+    [SerializeField] private AudioObject[] audioObjectsGame = new AudioObject[0];
+
+
+    #endregion
+
+    #region Properties
+
+    public AudioObject[] AudioObjectsMenu
+    {
+        get { return audioObjectsMenu; }
+        set { audioObjectsMenu = value; }
+    }
+
+    public AudioObject[] AudioObjectsGame
+    {
+        get { return audioObjectsGame; }
+        set { audioObjectsGame = value; }
+    }
 
     #endregion
 

48549c69d63fc58d06b7ccc32defea1806949a5e
Efgeen <efgeen1998@gmail.com>
2019-09-04 14:25:34 +0200

    Quickfix AudioData

--- GodVr/Assets/Scripts/Audio/AudioData.cs
+++ GodVr/Assets/Scripts/Audio/AudioData.cs
@@ -7,8 +7,11 @@ public class AudioData
 
     #region Fields
 
-    [SerializeField] private AudioObject[] audioObjectsMenu = null;
-    [SerializeField] private AudioObject[] audioObjectsGame = null;
+    [SerializeField]
+    private AudioObject[] audioObjectsMenu = null;
+
+    [SerializeField]
+    private AudioObject[] audioObjectsGame = null;
 
     #endregion
 

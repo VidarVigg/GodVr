0e3b9f86f9ffd5303ae3261f333aefb49ed0d0da
Anton <a.e.lindkvist@gmail.com>
2019-09-04 13:15:31 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ GodVr/Assets/Scripts/Editor/GroundItemEditor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(GroundItemScript))]
+[CanEditMultipleObjects]
+public class GroundItemEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        GroundItemScript script = (GroundItemScript)target;
+        if (GUILayout.Button("Ground Object"))
+        {
+            script.DropChildObjects();
+        }
+
+        if (GUILayout.Button("Undo Ground"))
+        {
+            script.Undo();
+        }
+    }
+
+
+}

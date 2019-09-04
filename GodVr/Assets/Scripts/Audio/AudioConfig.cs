4b4e4d74bd8aa277cedd44e037d5112bf8b00954
Efgeen <efgeen1998@gmail.com>
2019-09-04 14:24:37 +0200

    Quickfix AudioConfig

--- GodVr/Assets/Scripts/Audio/AudioConfig.cs
+++ GodVr/Assets/Scripts/Audio/AudioConfig.cs
@@ -1,20 +1,32 @@
-﻿using UnityEngine;
-using System;
+﻿using System;
+using UnityEngine;
 
 [Serializable]
 public class AudioConfig
 {
+
     #region Fields
 
-    [SerializeField] private MenuAudioSet menuAudioDefault;
-    [SerializeField] private GameAudioSet gameAudioDefault;
+    [SerializeField]
+    private MenuAudioSet menuAudioDefault = null;
+
+    [SerializeField]
+    private GameAudioSet gameAudioDefault = null;
 
     #endregion
 
     #region Properties
 
-    public MenuAudioSet MenuAudioSet { get { return menuAudioDefault; } }
-    public GameAudioSet GameAudioSet { get { return gameAudioDefault; } }
+    public MenuAudioSet MenuAudioSet
+    {
+        get { return menuAudioDefault; }
+    }
+
+    public GameAudioSet GameAudioSet
+    {
+        get { return gameAudioDefault; }
+    }
 
     #endregion
+
 }

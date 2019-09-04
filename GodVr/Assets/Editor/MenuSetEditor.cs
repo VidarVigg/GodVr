8596b1dbeca91e9e49c80ac457e2f43ae32b0483
Efgeen <efgeen1998@gmail.com>
2019-09-04 14:28:07 +0200

    Menu Set Editor, Testing Audio Master

---
+++ GodVr/Assets/Editor/MenuSetEditor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(MenuAudioSet))]
+public class MenuSetEditor : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+
+        MenuAudioSet gas = (MenuAudioSet)target;
+
+        //for (int i = 0; i < gas.AudioObjects.Length; i++)
+        //{
+        //    EditorGUILayout.ObjectField(gas.AudioObjects[i].AudioType.ToString(), gas.AudioObjects[i].AudioClip, typeof(AudioClip), allowSceneObjects: true);
+        //}
+
+    }
+
+}

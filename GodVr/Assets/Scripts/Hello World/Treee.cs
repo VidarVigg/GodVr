8b0e05f04072b8d75ee98065eacb7b2a2c83c894
Heimer Ahnfelt <thekingofspades@live.se>
2019-09-10 11:36:07 +0200

    Uglycoded treee

---
+++ GodVr/Assets/Scripts/Hello World/Treee.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class Treee : NaturalMaster
+{
+
+    public override void Throw(SteamVR_Behaviour_Pose trackedObj)
+    {
+        Object.DestroyImmediate(joint);
+        joint = null;
+
+        Transform origin = trackedObj.origin ? trackedObj.origin : trackedObj.transform.parent;
+        if (origin != null)
+        {
+            rigi.velocity = origin.TransformVector(trackedObj.GetVelocity());
+            rigi.angularVelocity = origin.TransformVector(trackedObj.GetAngularVelocity());
+        }
+        else
+        {
+            rigi.velocity = trackedObj.GetVelocity();
+            rigi.angularVelocity = trackedObj.GetAngularVelocity();
+        }
+
+        rigi.maxAngularVelocity = rigi.angularVelocity.magnitude;
+        Debug.Log("I'm a tree being thrown");
+        rigi.useGravity = true;
+    }
+    //public override void Grab(Rigidbody attach)
+    //{
+    //    Debug.Log("I'm a tree being thrown");
+    //    rigi.useGravity = true;
+    //}
+}

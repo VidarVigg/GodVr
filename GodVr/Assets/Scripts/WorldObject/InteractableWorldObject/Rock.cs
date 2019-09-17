d752f4540aaa182ebb7145b17754d8eaa85323fc
Anton <a.e.lindkvist@gmail.com>
2019-09-17 11:05:48 +0200

    added a bunch of vfx for a rock impact

--- GodVr/Assets/Scripts/WorldObject/InteractableWorldObject/Rock.cs
+++ GodVr/Assets/Scripts/WorldObject/InteractableWorldObject/Rock.cs
@@ -8,4 +8,10 @@ public class Rock : NaturalMaster
     {
         return false;
     }
+
+    protected override void OnCollision(Collision collision)
+    {
+        //Destroy(gameObject);
+    }
+
 }

3aedc23705bacd75cfee602ee059b2d53561bb75
Heimer Ahnfelt <thekingofspades@live.se>
2019-09-20 15:15:33 +0200

    Instantiate treevfx in world, fixed treee script a bit (it's ugly now)

--- GodVr/Assets/Scripts/WorldObject/InteractableWorldObject/Treee.cs
+++ GodVr/Assets/Scripts/WorldObject/InteractableWorldObject/Treee.cs
@@ -30,16 +30,29 @@ public class Treee : NaturalMaster
 
         if (rigi.velocity.sqrMagnitude >= 0.05)
         {
-            treeeConfig.ParticleSystem.Play();
-            Debug.Log(collider.gameObject);
-
-            Debug.Log("tree was moving");
             if (collider.gameObject.layer == 10)
             {
-                Debug.Log("Enemy");
-                treeeConfig.ParticleSystem.Play();
-                Destroy(gameObject, 1f);
+                if (treeeConfig.ParticleSystem)
+                {
+                    var obj = Object2.Instantiate<GameObject>(treeeConfig.ParticleSystem, collider.transform, true);
+                    obj.transform.SetParent(null);
+                    obj.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+                    obj.SetActive(true);
+                    ServiceLocator.TestAudioMasterService.PlayOneShot(AudioType.SFXEnemyImpact, treeeConfig.AudioSource);
+                    Destroy(obj, 2f);
+                    Destroy(gameObject);
+                }
             }
+            //treeeConfig.ParticleSystem.Play();
+            //Debug.Log(collider.gameObject);
+
+            //Debug.Log("tree was moving");
+            //if (collider.gameObject.layer == 10)
+            //{
+            //    Debug.Log("Enemy");
+            //    treeeConfig.ParticleSystem.Play();
+            //    Destroy(gameObject, 1f);
+            //}
         }
         else
         {

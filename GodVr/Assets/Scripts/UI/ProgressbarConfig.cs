21c2ca6438d6dbef24979300b68384eb1ab029ca
Efgeen <efgeen1998@gmail.com>
2019-09-24 11:41:20 +0200

    1234567

---
+++ GodVr/Assets/Scripts/UI/ProgressbarConfig.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+[Serializable]
+public class ProgressbarConfig
+{
+
+    #region Fields
+
+    [SerializeField]
+    private Image foreground = null;
+
+    [SerializeField]
+    private Image background = null;
+
+    [SerializeField]
+    private Image outline = null;
+
+    [SerializeField]
+    private TextMeshProUGUI text = null;
+
+    #endregion
+
+    #region Properties
+
+    public Image Foreground
+    {
+        get { return foreground; }
+    }
+
+    public Image Background
+    {
+        get { return background; }
+    }
+
+    public Image Outline
+    {
+        get { return outline; }
+    }
+
+    public TextMeshProUGUI Text
+    {
+        get { return text; }
+    }
+
+    #endregion
+
+}
---
+++ GodVr/Assets/Scripts/UI/ProgressbarMaster.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressbarMaster : MonoBehaviour
+{
+
+    #region Fields
+
+    [Header("Config"), SerializeField]
+    private ProgressbarConfig progressbarConfig = null;
+
+    [Header("Data"), SerializeField]
+    private ProgressbarData progressbarData = null;
+
+    private ProgressbarController progressbarController = null;
+
+    #endregion
+
+    #region Methods
+
+    private void Awake()
+    {
+        progressbarController = new ProgressbarController(this, progressbarConfig, progressbarData);
+    }
+
+    public void SetValue(float value)
+    {
+        progressbarController.SetValue(value);
+    }
+
+    #endregion
+
+}

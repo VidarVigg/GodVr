a0cb03ea8160efe1f26ea4e709a056e30ed725aa
Anton <a.e.lindkvist@gmail.com>
2019-09-25 16:30:09 +0200

    updated prefabs and prefab overrides + left menu script

---
+++ GodVr/Assets/Scripts/Currently Unsorted Scripts/LeftRadialMenuOptions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LeftRadialMenuOptions : MonoBehaviour
+{
+
+    #region Debug Methods
+    public void ReloadScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+    }
+
+    public void LogDebugLog()
+    {
+        Debug.Log("Debug_Clicked");
+    }
+    #endregion
+}

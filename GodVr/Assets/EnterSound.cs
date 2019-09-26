d5b73ff4b11f342dc886d924fe832a3601403080
VidarVigg <vidar.martensson@outlook.com>
2019-09-26 16:17:14 +0200

    Added Scream Sounds

---
+++ GodVr/Assets/EnterSound.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterSound : MonoBehaviour
+{
+    [SerializeField] private AudioSource audioSource;
+
+    private float tick;
+    private float max = 10;
+
+    private void Update()
+    {
+        tick += Time.deltaTime;
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<UnitAIMaster>())
+        {
+            if (tick > max)
+            {
+                ServiceLocator.TestAudioMasterService.PlayOneShot(AudioType.SFXScream, audioSource);
+                tick = 0;
+            }
+        }
+    }
+}
